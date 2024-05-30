99cf8a0964fbeb0f8906af7ed8d6a10512bd3173
Amp <110274160+Amphvptere@users.noreply.github.com>
2024-05-30 00:05:18 -0600

    Update BackMain.js

--- BackMain.js
+++ BackMain.js
@@ -1,4 +1,3 @@
-
 let h, w
 h = document.documentElement.clientHeight
 w = document.documentElement.clientWidth
@@ -6,11 +5,23 @@ w = document.documentElement.clientWidth
 console.log(h, w)
 
 
+
 function loadClick() {
     let textInput = document.getElementById('textInput')
 
     document.getElementById('load').style.display = "none";
+    document.querySelector('.center').style.display = "none";
     textInput.style.display = "block";
-    textInput.rows = h/19
+
+
+    let r = h/19
+    for (let i = 0; i < r; i++) {
+        let newRow = document.createElement("p")
+        newRow.innerHTML = i+1; newRow.id = "colItem"; newRow.style.height = String(textInput.clientHeight/r).concat("px");
+        console.log(String(textInput.clientHeight/r).concat("px"))
+        document.getElementById('col').appendChild(newRow)
+    }
+
+    textInput.rows = r
     textInput.cols = w/10
 }
